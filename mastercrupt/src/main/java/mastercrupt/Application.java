d3de0c22cfe725825c0ef342b3fa5343b193ed31
Rachel <rachelcarmena@users.noreply.github.com>
2019-04-15 21:24:01 +0200

    Refactor: inline method

--- mastercrupt/src/main/java/mastercrupt/Application.java
+++ mastercrupt/src/main/java/mastercrupt/Application.java
@@ -6,7 +6,7 @@ import ui.UI;
 public class Application implements ApplicationInterface {
     @Override
     public void leet(String string, UI ui) {
-        ui.setLeeted(Leeter.leet(string));
+        ui.setLeeted(string.replace('e', '3'));
     }
     public static void main(String[] args) {
         UI ui = new UI(new Application());
